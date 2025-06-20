9c4f298e1fdb750bbcfe438b9bff2b3e226cd89d
DanielPaulGriffin <59080776+DanielPaulGriffin@users.noreply.github.com>
2025-06-21 09:06:06 +1000

    Update colors.js

--- modules/colors.js
+++ modules/colors.js
@@ -1,5 +1,6 @@
 export const lineColor = '#484848';
 export const polyFillColor = '#050505';
-export const bgColor = '#181818';
+export const bgColor = '#050505';
 //'#484848' - grey
+//'#181818' - light grey
 //'#16f110' - green
