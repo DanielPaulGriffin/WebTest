7dfe6a7ecad0ec182c98b9323ce19f2c80869796
DanielPaulGriffin <59080776+DanielPaulGriffin@users.noreply.github.com>
2025-06-21 08:10:31 +1000

    Update main.js

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ function gameLoop(timestamp) {
     updateParticles(particles);
     
     // Check collisions
-    checkCollisions(rocket, levels[currentLevel%levels.length], particles, scoreElement,resetGame,levelWon);
+    checkCollisions(rocket, levels[(currentLevel%levels.length)-1], particles, scoreElement,resetGame,levelWon);
     
     // Increase score over time
     rocket.score += deltaTime * 0.001;
